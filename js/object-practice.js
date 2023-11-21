f2e3ef8d6ed6aba1167682fa5d2eb9551efd73e1
manyakbot <skylinedelfly@gmail.com>
2023-11-21 20:01:04 +0600

    !!!

---
+++ js/object-practice.js
@@ -0,0 +1,31 @@
+let myPC = {
+    monitor: {
+        features: {
+            name: 'acer',
+            screenResolution: 1920,
+            color: 'white',
+        }
+    },
+    keyboard: {
+        name: 'unknown',
+        color: 'black',
+    },
+    mouse: {
+        name: 'unknown',
+        color: 'black',
+        own: 'Middle School 9'
+    },
+    systemUnit: {
+        components: {
+            videoCard: 'Radeon 6900xt',
+            ram: 32,
+            motherboard: 'MSI b550',
+            processor: 'AMD 5800',
+            waterCooling: 'MSI',
+            unitPower: 1000 + 'W',
+            hardDisk: 'SSD ' + 1 + 'TB',
+        }
+    },
+}
+
+console.log(myPC);
