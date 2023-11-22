7a8750e6bf98fe4801f5b98cc4e1a9b6e9375fc0
manyakbot <skylinedelfly@gmail.com>
2023-11-22 10:48:03 +0600

    1

--- js/object-practice-2.js
+++ js/object-practice-2.js
@@ -7,3 +7,14 @@ const dogs = {
 const myDog = 'Hunter';
 const myBreed = dogs[myDog];
 console.log(myBreed);
+
+const myFriends = {
+    FriendOne: 'Dima',
+    FriendTwo: 'Stas',
+    FriendThree: 'Kot4',
+}
+
+const friend = 'FriendTwo';
+const newFriend = myFriends[friend];
+console.log(typeof newFriend);
+console.log(typeof myFriends.FriendTwo);
