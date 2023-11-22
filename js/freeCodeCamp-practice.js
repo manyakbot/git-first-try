a023c6ff8f36421c7f0a41fec5dd38cd5cc50013
manyakbot <skylinedelfly@gmail.com>
2023-11-22 11:10:30 +0600

    practice

---
+++ js/freeCodeCamp-practice.js
@@ -0,0 +1,13 @@
+const phoneticLookup = (val) => {
+    let result = "";
+
+    const lookup = {
+        alpha: "Adams",
+        bravo: "Boston",
+        delta: "Denver",
+        echo: "Easy",
+        foxtrot: "Frank",
+    }
+    return result;
+}
+console.log(phoneticLookup(2));
