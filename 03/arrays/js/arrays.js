b7e0c4b938aee4cf35ff53462d23fcb5888a6480
manyakbot <skylinedelfly@gmail.com>
2023-11-22 14:49:53 +0600

    i also hate arrays

--- 03/arrays/js/arrays.js
+++ 03/arrays/js/arrays.js
@@ -1,5 +1,18 @@
+
+
 const books = [[['War and love'], [], []],
 [[], [], ['Master and Margarita']],
 [[], ['Crime and Punishment'], []]];
 
-console.log(books[0][0], books[5][0], books[7][0]);
+
+for(i = 0; i < books.length; i++) {
+    for(j = 0; j < books.length; j++){
+        if(books[i][j].length > 0){
+            console.log(books[i][j])
+        }
+        else {
+            console.log('If you see it, congratz you find empty array');
+        }
+    }
+}
+console.log(books[0][0][0], books[1][2][0], books[2][1][0]);
