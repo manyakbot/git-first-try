45417cb1d5253dfef5c56a8524ee7bfee2606098
manyakbot <skylinedelfly@gmail.com>
2023-11-23 19:44:09 +0600

    123

--- 03/arrays/js/justPractice.js
+++ 03/arrays/js/justPractice.js
@@ -1,26 +1,3 @@
-const myMusic = [
-    {
-      "artist": "Billy Joel",
-      "title": "Piano Man",
-      "release_year": 1973,
-      "formats": [
-        "CD",
-        "8T",
-        "LP"
-      ],
-      "gold": true
-    }
-  ];
+const str = "hEY, hOW are yOU?";
 
-  myMusic.add = {
-    "artist" : "Jean",
-    "title" : "Guitar",
-    "release_year" : 2000,
-    "formats" : [
-      "DVD"
-    ],
-    "gold": true
-};
-
-    
-    console.log(myMusic);
+console.log(str.slice(0,1).toUpperCase() + str.slice(1).toLowerCase());
