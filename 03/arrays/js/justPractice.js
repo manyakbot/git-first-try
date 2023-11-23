09d82b1b3a376e4db9abc902d618c23b6d71e1c5
manyakbot <skylinedelfly@gmail.com>
2023-11-23 21:06:04 +0600

    i am also so stuipid

--- 03/arrays/js/justPractice.js
+++ 03/arrays/js/justPractice.js
@@ -1,8 +1,6 @@
-const concatArrays = (array0, array1) => {
-    return array0.concat(array1);
-} 
-
-const array = [1,2,3,4,5];
-const array2 = [6,7,8,9,10]
-const arr = concatArrays(array, array2); 
-console.log(arr);
+const searchUnicalElements = (array) => {
+    return array.filter((item, index) => array.indexOf(item) === index);
+}
+const arr = [1,2,3,4,5,5,2,3,4,6];
+const callArr = searchUnicalElements(arr);
+console.log(callArr);
