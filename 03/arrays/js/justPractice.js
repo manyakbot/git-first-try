9bb6c5deca51fb073943afb7baf692a71ea5727f
manyakbot <skylinedelfly@gmail.com>
2023-11-23 20:30:29 +0600

    1234

--- 03/arrays/js/justPractice.js
+++ 03/arrays/js/justPractice.js
@@ -1,3 +1,8 @@
-const str = "hEY, hOW are yOU?";
-
-console.log(str.slice(0,1).toUpperCase() + str.slice(1).toLowerCase());
+const array0 = [1, 2, 3, 4, 5, 7];
+for (i = 0; i < array0.length; i++) {
+    if (array0.indexOf(4) == 3) {
+        console.log(i)
+    }else {
+        console.log('hi')
+    }
+}
