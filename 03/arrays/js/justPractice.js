995f979cc04459431f9aa09068b6b3c5d0395f1c
manyakbot <skylinedelfly@gmail.com>
2023-11-23 21:11:04 +0600

    so stupid

--- 03/arrays/js/justPractice.js
+++ 03/arrays/js/justPractice.js
@@ -1,6 +1,16 @@
-const searchUnicalElements = (array) => {
-    return array.filter((item, index) => array.indexOf(item) === index);
+const objectsOfArray = [{
+    name: 'Stas',
+    age: 26
+}, {
+    name: 'Oleg',
+    age: 24
+}, {
+    name: 'Dima',
+    age: 24
+},];
+
+const sortOfFunc = (obj) => {
+    return obj.sort((a, b) => a.age - b.age) // сортирует по возростанию
 }
-const arr = [1,2,3,4,5,5,2,3,4,6];
-const callArr = searchUnicalElements(arr);
-console.log(callArr);
+const newObject = sortOfFunc(objectsOfArray);
+console.log(newObject);
