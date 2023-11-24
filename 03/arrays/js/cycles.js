82b2367f77479f65ea9de175fd29e86c90acd782
manyakbot <skylinedelfly@gmail.com>
2023-11-24 16:22:12 +0600

    train cycles

--- 03/arrays/js/cycles.js
+++ 03/arrays/js/cycles.js
@@ -1,6 +1,17 @@
-const grades = [5, 4, 3, 5, 4];
-const students = ['Misha', 'Sasha', 'Kasha', 'Oleg', 'Stas'];
-
-for (let grade = 0; grade < grades.length; grade++) {
-    document.write(students[grade] + " " + grades[grade], '<br>')
+const students = [{
+    name: 'Diana',
+    age: 13
+}, {
+    name: 'Margarrita',
+    age: 13
+}, {
+    name: 'Karolina',
+    age: 12
+}];
+let counter = 0;
+for(i = 0; i < students.length; i++) {
+    // document.write(students[i].name + " " + students[i].age + " years old", '<br>');
+    counter += students[i].age
+    
 }
+document.write(counter / students.length)
