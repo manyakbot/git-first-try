e456ece4a000fbc25522296d255de352546ca396
manyakbot <skylinedelfly@gmail.com>
2023-11-24 14:49:20 +0600

    practice cycles

--- 03/arrays/js/cycles.js
+++ 03/arrays/js/cycles.js
@@ -1,5 +1,12 @@
-let counter = 0;
-let showOnConsole = "a"
-for(i = 0; i < showOnConsole; i++) {
-    console.log(i);
+
+const drawCats = (howManyTimes) => {
+    let counter = 0;    
+    for(i = 0; i < howManyTimes; i++) {
+        // console.log(i + " =^.^=");
+        counter += i;
+    }
+    return counter / howManyTimes;
 }
+console.log(drawCats(10));
+console.log(drawCats(10));
+
