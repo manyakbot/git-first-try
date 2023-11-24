f274d6164c974ab2774fc4a78bf5b92632df102c
manyakbot <skylinedelfly@gmail.com>
2023-11-24 17:15:02 +0600

    142

--- 03/arrays/js/cycles.js
+++ 03/arrays/js/cycles.js
@@ -5,7 +5,7 @@ const shoppingList = [
 ];
 
 for (i = 0; i < shoppingList.length; i++) {
-    for(j = 0; j <shoppingList.length; i++){
+    for (j = 0; j < shoppingList[i].length; j++) {
         document.write(shoppingList[i][j], '<br>');
     }
 }
