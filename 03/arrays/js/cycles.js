b3d7ce5ffa69661943acc8d239994ea48699a04c
manyakbot <skylinedelfly@gmail.com>
2023-11-24 17:49:16 +0600

    wqe

--- 03/arrays/js/cycles.js
+++ 03/arrays/js/cycles.js
@@ -1,11 +1,68 @@
-const shoppingList = [
-    ['milk', 'bread', 'eggs'],
-    ['juice', 'ice cream'],
-    ['cat food', 'shampoo']
-];
-
-for (i = 0; i < shoppingList.length; i++) {
-    for (j = 0; j < shoppingList[i].length; j++) {
-        document.write(shoppingList[i][j], '<br>');
+const extractedGrades = [[{
+    name: 'Diana',
+    grades: {
+        chemistry: 90,
+        informatics: 95,
+        mathematics: 90,
+        physics: 90
     }
+}, {
+    name: 'Aigerim',
+    grades: {
+        chemistry: 95,
+        informatics: 90,
+        mathematics: 95,
+        physics: 90
+    }
+}, {
+    name: 'Aidana',
+    grades: {
+        chemistry: 90,
+        informatics: 95,
+        mathematics: 90,
+        physics: 90
+    }
+}],
+[{
+    name: 'Stas',
+    grades: {
+        chemistry: 90,
+        informatics: 95,
+        mathematics: 90,
+        physics: 90
+    }
+}, {
+    name: 'Dima',
+    grades: {
+        chemistry: 95,
+        informatics: 90,
+        mathematics: 95,
+        physics: 90
+    }
+}, {
+    name: 'Oleg',
+    grades: {
+        chemistry: 90,
+        informatics: 95,
+        mathematics: 90,
+        physics: 90
+    }
+}]];
+
+
+
+const func = (grades) => {
+    let counter = 0;
+    let totalStudents = 0;
+    for (i = 0; i < grades.length; i++) {
+        for (j = 0; j < grades[i].length; j++) {
+            counter += grades[i][j].grades.chemistry
+            totalStudents++;
+        }
+    }
+    return counter / totalStudents
 }
+console.log(func(extractedGrades));
+
+
+
