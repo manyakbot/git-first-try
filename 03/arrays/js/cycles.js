43115eae6cae538a413f8074381ce17275353fa6
manyakbot <skylinedelfly@gmail.com>
2023-11-24 21:50:13 +0600

    method map

--- 03/arrays/js/cycles.js
+++ 03/arrays/js/cycles.js
@@ -1,6 +1,10 @@
-const array = [1,2,3,4,5];
-const func = (arr) => {
-    return arr.map(item => 10*item)
-}
-const callArray = func(array);
-console.log(callArray);
+// const array = [1,2,3,4,5];
+// const func = (arr) => {
+//     return arr.map(item => 10*item)
+// }
+// const callArray = func(array);
+// console.log(callArray);
+
+const arr0 = [1,2,3,4,5];
+const convertArray = arr0.map(item => " " + item);
+console.log(convertArray);
