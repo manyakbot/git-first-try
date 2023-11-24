1238772a36cef091505529847c4715a69359b0c9
manyakbot <skylinedelfly@gmail.com>
2023-11-24 21:59:57 +0600

    123

--- 03/arrays/js/cycles.js
+++ 03/arrays/js/cycles.js
@@ -5,6 +5,14 @@
 // const callArray = func(array);
 // console.log(callArray);
 
-const arr0 = [1,2,3,4,5];
-const convertArray = arr0.map(item => " " + item);
-console.log(convertArray);
+// const arr0 = [1,2,3,4,5];
+// const convertArray = arr0.map(item => " " + item);
+// console.log(convertArray);
+
+const newObject = [
+    {name: "James", age: 17}, 
+    {name: "Aida", age: 12}
+];
+const convertArr = newObject.map(item => item.name);
+
+console.log(convertArr);
