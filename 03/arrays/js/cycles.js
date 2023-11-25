84c552be61f89e76c877dd2f65c31efa63ef4ea8
manyakbot <skylinedelfly@gmail.com>
2023-11-25 19:12:22 +0600

    dima Loh

--- 03/arrays/js/cycles.js
+++ 03/arrays/js/cycles.js
@@ -1,29 +1,14 @@
-// const array = [1,2,3,4,5];
-// const func = (arr) => {
-//     return arr.map(item => 10*item)
-// }
-// const callArray = func(array);
-// console.log(callArray);
+const ourStorage = {
+    "desk": {
+      "drawer": "stapler"
+    },
+    "cabinet": {
+      "top drawer": { 
+        "folder1": "a file",
+        "folder2": "secrets"
+      },
+      "bottom drawer": "soda"
+    }
+  };
 
-// const arr0 = [1,2,3,4,5];
-// const convertArray = arr0.map(item => " " + item);
-// console.log(convertArray);
-
-// const newObject = [
-//     {name: "James", age: 17}, 
-//     {name: "Aida", age: 12}
-// ];
-// const convertArr = newObject.map(item => item.name);
-
-// console.log(convertArr);
-
-// const massiv = ['oleg', 'stas', 'dima'];
-// const count = massiv.map(item => item.length);
-// console.log(count);
-
-const arr1 = ['qwe', 'asdwq', 'asdwq'];
-const arr2 = ['asd', 'asdwq', 'asdwr'];
-
-const concatArrays = arr1.map((item, index) => item.concat(arr2[index]));
-
-console.log(concatArrays);
+  console.log(ourStorage.cabinet['top drawer'].folder1);
