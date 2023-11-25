f118f7a6583125faabfee920251270d7e6b06cad
manyakbot <skylinedelfly@gmail.com>
2023-11-25 20:29:23 +0600

    4123421

--- 03/arrays/js/cycles.js
+++ 03/arrays/js/cycles.js
@@ -1,3 +1,36 @@
+// const libraryCollection = {
+//     1: {
+//         title: "Flame",
+//         writer: "Jon",
+//         series: [1999, 'Autumn']
+//     },
+//     2: {
+//         writer: "Amanda",
+//         series: []
+//     },
+//     3: {
+//         title: "Wind",
+//     }
+// }
+
+// const updateLibrary = (library, id, property, value) => {
+//     if(value === "") {
+//         delete library[id][property];
+//     }else if(property !== "series") {
+//         library[id][property] = value;
+//     }else if(property == "series"){
+//         if(!library[id].hasOwnProperty("series")){
+//             library[id]["series"] = [];
+//         }
+//         library[id]["series"].push(value);
+//     }
+    
+    
+//     return library;
+// }
+
+// console.log(updateLibrary(libraryCollection, 3, 'writer', 'ABBA'));
+
 // Setup
 const recordCollection = {
     2548: {
@@ -21,20 +54,17 @@ const recordCollection = {
   
   // Only change code below this line
   function updateRecords(records, id, prop, value) {
-    
-    if(value === "") {
-        delete records[id][prop]
-    }
-    else if (prop !== "tracks") {
-        records[id]["tracks"] = records[id]["albumTitle"];
-    }
-    else if (prop === "tracks") {
+    if(value === ""){
+        delete records[id][prop];
+    }else if(prop !== "tracks"){
         records[id][prop] = value;
+    }else if(prop === "tracks"){
+        if(!records[id].hasOwnProperty("tracks")){
+            records[id]["tracks"] = [];
+        }
+        records[id]["tracks"].push(value);
     }
-
-
-    
     return records;
   }
   
-  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+  console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));
