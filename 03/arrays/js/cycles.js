86e1f930667373b2824c44a10d1dc28293d21c37
manyakbot <skylinedelfly@gmail.com>
2023-11-25 19:45:52 +0600

    123

--- 03/arrays/js/cycles.js
+++ 03/arrays/js/cycles.js
@@ -1,22 +1,40 @@
-const myPlants = [
-    {
-      type: "flowers",
-      list: [
-        "rose",
-        "tulip",
-        "dandelion"
-      ]
+// Setup
+const recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
     },
-    {
-      type: "trees",
-      list: [
-        "fir",
-        "pine",
-        "birch"
-      ]
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
     }
-  ];
+  };
   
-  const secondTree = myPlants[1].list[1];
-  console.log(secondTree);
+  // Only change code below this line
+  function updateRecords(records, id, prop, value) {
+    
+    if(value === "") {
+        delete records[id][prop]
+    }
+    else if (prop !== "tracks") {
+        records[id]["tracks"] = records[id]["albumTitle"];
+    }
+    else if (prop === "tracks") {
+        records[id][prop] = value;
+    }
 
+
+    
+    return records;
+  }
+  
+  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
