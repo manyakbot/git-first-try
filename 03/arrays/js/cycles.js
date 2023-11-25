e13eb9834cb3d042c35c50b4856c9561ef1d7307
manyakbot <skylinedelfly@gmail.com>
2023-11-25 19:14:57 +0600

    213

--- 03/arrays/js/cycles.js
+++ 03/arrays/js/cycles.js
@@ -1,14 +1,14 @@
-const ourStorage = {
-    "desk": {
-      "drawer": "stapler"
-    },
-    "cabinet": {
-      "top drawer": { 
-        "folder1": "a file",
-        "folder2": "secrets"
-      },
-      "bottom drawer": "soda"
+const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
     }
   };
-
-  console.log(ourStorage.cabinet['top drawer'].folder1);
+  
+  const gloveBoxContents = myStorage.car.inside['glove box'];
+  console.log(gloveBoxContents);
