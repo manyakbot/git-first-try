7b34f8ee977452a9edee986623588589a14294ac
manyakbot <skylinedelfly@gmail.com>
2023-11-27 16:34:04 +0600

    asdas

--- 03/arrays/js/cycles.js
+++ 03/arrays/js/cycles.js
@@ -1,8 +1,14 @@
-// Setup
-const myArray = [];
-
-for(let i = 1; i < 10; i += 2) {
-  myArray.push(i)
-}
-// Only change code below this line
-console.log(myArray);
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+  
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j]
+    }
+  }
+    // Only change code above this line
+    return product;
+  }
+  
+  console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]]));
