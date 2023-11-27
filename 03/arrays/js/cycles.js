01dac169b6b77403cebceec8151df7640de9ea1f
manyakbot <skylinedelfly@gmail.com>
2023-11-27 15:41:38 +0600

    asdasdasdasd

--- 03/arrays/js/cycles.js
+++ 03/arrays/js/cycles.js
@@ -1,5 +1,7 @@
-const array = [1,2,34,5,4,3,34,];
-function findEddElement(arr) {
-    return arr.filter(item => item % 2 == 0)
+const array = [2,234,42,32];
+const elementOfArray = array[2];
+const func = (arr, element ) => {
+    return arr.indexOf(element);
 }
-console.log(findEddElement(array));
+
+console.log(func(array, elementOfArray));
