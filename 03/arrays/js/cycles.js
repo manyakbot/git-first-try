c57f09adf75b4be815f9919eba85a4964837125d
manyakbot <skylinedelfly@gmail.com>
2023-11-27 15:27:39 +0600

    123

--- 03/arrays/js/cycles.js
+++ 03/arrays/js/cycles.js
@@ -1,31 +1,11 @@
-const listOfStudents = {
-    44: {
-        name: 'Oleg',
-        age: 16,
-        course: 'english'
-    },
-    46: {
-        name: 'Stas',
-        age: 17,
-        course: 'italiano'
-    },
-    55: {
-        name: 'Dima',
-        age: 15,
-        course: 'german'
-    },
-}
+const arr0 =  [1,2,3,4,5];
 
-function updateData (list, id, property, value) {
-        if(value === "Stas"){
-            delete list[id][property];
-        }else if(value === "Stas"){
-            list[id][property] = "Kon4a";
-        }else if(value !== "Stas"){
-            list[id][property] = value;
-        }
-    return list;
+const sumOfArray =(array) => {
+    let counter = 0;
+    for(i = 0; i < array.length; i++) {
+        counter += array[i];
+        
+    }
+    return counter;
 }
-
-const result = updateData(listOfStudents, 46, name, "Krasav4ik");
-console.log(result);
+console.log(sumOfArray(arr0));
