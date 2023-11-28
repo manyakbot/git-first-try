1009ac559c5772245c7cb379b6bcc1c90c7ef02c
manyakbot <skylinedelfly@gmail.com>
2023-11-28 13:09:37 +0600

    asdasd

--- 03/arrays/js/cycles.js
+++ 03/arrays/js/cycles.js
@@ -1,9 +1,21 @@
-const name = 'Stas';
-const age = 26;
+const inEnglish = 'Hello';
+const inRussian = 'Привет';
 
-const userProfile = {
-    name: name,
-    age: age,
-    isHeAGay: true
+// const greet = {
+//     inEnglish, //
+//     inRussian, //для такой сокращенной записи название свойство объекта и переменной должны быть одинаковы
+//     inKazakh() {
+//         console.log('Саламатсызба!');
+//     }
+// }
+// greet.inKazakh();
+
+const greet = {
+    inEnglish, inEnglish,
+    inRussian, inRussian, 
+    inKazakh() {
+        console.log('Саламатсызба!');
+    }
 }
-console.log(userProfile);
+greet.inKazakh();
+console.log(greet.inEnglish, greet.inRussian)
