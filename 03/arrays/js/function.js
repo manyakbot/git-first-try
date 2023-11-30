f18201626a00dd26be0963e459cd30818d793ead
manyakbot <skylinedelfly@gmail.com>
2023-11-30 10:10:07 +0600

    wqeqwe

--- 03/arrays/js/function.js
+++ 03/arrays/js/function.js
@@ -5,17 +5,7 @@ const result = func(10, 5); // func(10, 5) это вызов функции  с 
 console.log(result);
 
 
-
-function positiveSum(arr) {
-    let counter = 0;
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i] > 0) {
-          counter += arr[i];
-        }
-    }
-    return counter;
-  }
-
-  const array = [1,-4,2,12];
-  const currentArray = positiveSum(array);
-  console.log(currentArray);
+function greet() {
+    console.log('hello world');
+}
+console.log(greet())
