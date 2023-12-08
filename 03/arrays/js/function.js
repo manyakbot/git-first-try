7b2271eb0811935337c6b20911c0e8b1ff3886ce
manyakbot <skylinedelfly@gmail.com>
2023-12-08 16:56:53 +0600

    asdasdasdasdas

--- 03/arrays/js/function.js
+++ 03/arrays/js/function.js
@@ -1,10 +1,39 @@
-const fc = (array) => {
-  return Math.max(...array)
-}
-const array = [1,2,3,4,5];
-const resultFc = fc(array);
-console.log(resultFc);
+const school = [
+  {
+    name: "Middle School 9",
+    addres: "Mira 14B",
+    images: ["https://mektep.online.kz", "https://instagram/mektep.kz"],
+    class: [
+      {
+        title: "1B",
+        studentsCount: 30,
+        rank: 1
+      },
+      {
+        title: "2A",
+        studentsCount: 26,
+        rank: null
+      },
+      {
+        title: "4G",
+        studentsCount: 29,
+      },
+    ],
+  },
+];
 
-setTimeout(()=> {
-  console.log(fc(array));
-}, 1000);
+const beginnerClass = school[0].class[0].title;
+const countStudensOfClass = school[0]['class'][0]['studentsCount'];
+
+
+const schoolRank = school[0].class;
+
+for(let i = 0; i < schoolRank.length; i++) {
+    if(!(schoolRank[i].hasOwnProperty('rank'))){
+      schoolRank[i]['rank'] = 4;
+    }else if(schoolRank[i]['rank'] === null) {
+      schoolRank[i]['rank'] = 6;
+    }
+}
+
+console.log(schoolRank);
